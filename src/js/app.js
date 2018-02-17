bee4df1fa66bed65fc95d9b0b8cf933abc8ed84f
Bharadwaj <bparthas@buffalo.edu>
2018-02-17 14:35:33 -0500

    code clean up and ui changes

--- src/js/app.js
+++ src/js/app.js
@@ -26,24 +26,21 @@ App = {
   initContract: function() {
       $.getJSON('Coin.json', function(data) {
     // Get the necessary contract artifact file and instantiate it with truffle-contract
-    var voteArtifact = data;
-    App.contracts.vote = TruffleContract(voteArtifact);
+        var voteArtifact = data;
+        App.contracts.vote = TruffleContract(voteArtifact);
 
     // Set the provider for our contract
-    App.contracts.vote.setProvider(App.web3Provider);
+        App.contracts.vote.setProvider(App.web3Provider);
 
     // Use our contract to retrieve and mark the voted pets
-    // return App.markvoted();     // TOD
-    //App.getMinter().then(function(){
-      App.getMinter();
-      App.currentAccount = web3.eth.coinbase;
-     jQuery('#current_account').text("Current account : "+web3.eth.coinbase);
-      App.handleEvents();
-    return App.bindEvents();
-    //});
-
-  });
-    //return App.bindEvents();
+    
+        App.getMinter();
+        App.currentAccount = web3.eth.coinbase;
+        jQuery('#current_account').text("Current account : "+web3.eth.coinbase);
+        jQuery('#curr_account').text(web3.eth.coinbase);
+        App.handleEvents();
+      return App.bindEvents();
+      });
   },
 
   bindEvents: function() {
@@ -54,91 +51,96 @@ App = {
   },
 
 
-populateAddress : function(){
- new Web3(new Web3.providers.HttpProvider('http://localhost:9545')).eth.getAccounts((err, accounts) => {
-  jQuery.each(accounts,function(i){
-    var optionElement = '<option value="'+accounts[i]+'">'+accounts[i]+'</option';
-    jQuery('#enter_create_address').append(optionElement);
-    jQuery('#enter_send_address').append(optionElement);
-  });
+  populateAddress : function(){
+ 
+    new Web3(new Web3.providers.HttpProvider('http://localhost:9545')).eth.getAccounts((err, accounts) => {
+      jQuery.each(accounts,function(i){
+        if(web3.eth.coinbase != accounts[i]){  
+          var optionElement = '<option value="'+accounts[i]+'">'+accounts[i]+'</option';
+          jQuery('#enter_create_address').append(optionElement);
+          jQuery('#enter_send_address').append(optionElement);  
+        }
+      });
+    });
+  },
 
-
-
-  });
-},
-
-handleEvents : function(){
-  var coinInstance;
-  App.contracts.vote.deployed().then(function(instance) {
-    coinInstance = instance;
-    var option = {
-      "fromBlock": "latest"
-    };
-    var event = coinInstance.allEvents(option);
-    event.watch(function(error,result) {
-
-      if(result.transactionHash != App.transaction){
-        App.transaction = result.transactionHash;
-        var text = 'Coin transfer: ' + result.args.amount +
-            ' coins were sent from ' + result.args.from +
-            ' to ' + result.args.to + '.';
-        jQuery('#showmessage_text').html(text);
-        jQuery('#show_event').animate({'right':'10px'});
-        setTimeout(function(){jQuery('#show_event').animate({'right':'-410px'},500)}, 5000);
-
-      }
-
-})
-});
-},
-
-getMinter : function(){
-  App.contracts.vote.deployed().then(function(instance) {
-    return instance.minter();
-}).then(function(result) {
-    App.minter = result;
-    jQuery('#minter').text("Minter : "+result);
-})
-
-},
-
-handleCreateMoney: function(addr,value){
-
-    if(App.currentAccount != App.minter){
-      alert("Not Authorised to create money");
-      return false;
-    }
-
+  handleEvents : function(){
     var coinInstance;
-
     App.contracts.vote.deployed().then(function(instance) {
       coinInstance = instance;
+      var option = {
+        "fromBlock": "latest"
+      };
+      var event = coinInstance.allEvents(option);
+      event.watch(function(error,result) {
+        console.log("yes event");
+        if(result.transactionHash != App.transaction){
+          App.transaction = result.transactionHash;
+          var text = 'Coin transfer: ' + result.args.amount +
+              ' coins were sent from ' + result.args.from +
+              ' to ' + result.args.to + '.';
+          jQuery('#showmessage_text').html(text);
+          jQuery('#show_event').animate({'right':'10px'});
+          setTimeout(function(){jQuery('#show_event').animate({'right':'-410px'},500)}, 5000);
 
-      return coinInstance.mint(addr,value);
-    }).then( function(result){
-      if(result.receipt.status == '0x01')
-        alert(value +" unit/units of amount Created successfully to "+addr);
-      else
-        alert("Creation Failed")
-    }).catch( function(err){
-      console.log(err.message);
+        }
+      })
+    });
+  },
+
+  getMinter : function(){
+    App.contracts.vote.deployed().then(function(instance) {
+      return instance.minter();
+    }).then(function(result) {
+      App.minter = result;
+      jQuery('#minter').text("Minter : "+result);
+      if(App.minter != App.currentAccount){
+        jQuery('#create_coin').css('display','none');
+        jQuery('#send_coin').css('width','50%');
+        jQuery('#balance_coin').css('width','50%');
+      }else{
+        jQuery('#create_coin').css('display','block');
+        jQuery('#send_coin').css('width','30%');
+        jQuery('#balance_coin').css('width','30%');
+      }
     })
+  },
 
-},
+  handleCreateMoney: function(addr,value){
 
-// handling the vote
+      if(App.currentAccount != App.minter){
+        alert("Not Authorised to create coin");
+        return false;
+      }
+
+      var coinInstance;
+
+      App.contracts.vote.deployed().then(function(instance) {
+        coinInstance = instance;
+
+        return coinInstance.mint(addr,value);
+      }).then( function(result){
+        if(result.receipt.status == '0x01')
+          alert(value +" coins created successfully to "+addr);
+        else
+          alert("Creation failed")
+      }).catch( function(err){
+        console.log(err.message);
+      })
+  },
+
+
   handleSendMoney: function(addr,value) {
 
     var coinInstance;
     App.contracts.vote.deployed().then(function(instance) {
       coinInstance = instance;
-
       return coinInstance.transfer(addr,value);
     }).then( function(result){
       if(result.receipt.status == '0x01')
-        alert(value +" unit / units of amount tranfered successfully transfered to "+addr);
+        alert(value +" coins tranfered successfully transfered to "+addr);
       else
-        alert("Transfer failed");
+          alert("Transfer failed");
     }).catch( function(err){
       console.log(err.message);
     })
@@ -147,12 +149,10 @@ handleCreateMoney: function(addr,value){
   checkBalance : function(){
     App.contracts.vote.deployed().then(function(instance) {
       coinInstance = instance;
-    return coinInstance.balances(App.currentAccount);
-}).then(function(result) {
-  alert("Current Balance : "+result.toNumber());
-})
-
-
+      return coinInstance.balances(App.currentAccount);
+    }).then(function(result) {
+      alert("Current Balance : "+result.toNumber());
+    })
   }
 };
 
